70f3141ab8811983186a362c22072cd797fd0b3e
ramyalakhani2108 <lakhani.ramya.u@gmail.com>
2025-02-20 16:45:45 +0530

    2

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as crypto from 'crypto';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
